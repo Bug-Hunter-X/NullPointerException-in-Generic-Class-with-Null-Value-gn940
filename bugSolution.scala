160351a7dcbabef7afbfd96e845a3b954660d766
BugHunter-X <askzenai@gmail.com>
2024-12-20 03:24:06 +0100

    Add bugSolution.scala file

---
+++ bugSolution.scala
@@ -0,0 +1,29 @@
+```scala
+class MyClass[T](val value: T) {
+  def printValue(): Unit = {
+    value match {
+      case null => println("Value is null")
+      case _ => println(value)
+    }
+  }
+}
+
+object MainObject {
+  def main(args: Array[String]): Unit = {
+    val myInt = new MyClass(10)
+    myInt.printValue() //Prints 10
+
+    val myString = new MyClass("Hello")
+    myString.printValue() //Prints Hello
+
+    val myList = new MyClass(List(1, 2, 3))
+    myList.printValue() //Prints List(1, 2, 3)
+
+    val myOption = new MyClass(Some(42))
+    myOption.printValue() //Prints Some(42)
+
+    val myNull = new MyClass(null)
+    myNull.printValue() //Prints "Value is null"
+  }
+}
+```
